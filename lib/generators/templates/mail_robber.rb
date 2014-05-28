78de0f03317b771df51d7e3e186ba6d2f344fd69
Ilya Gelman <ilyagelman@ilyagelman.ru>
2014-05-28 15:05:40 +0300

    added comments and changed defaults on generated initializer

--- lib/generators/templates/mail_robber.rb
+++ lib/generators/templates/mail_robber.rb
@@ -1,5 +1,18 @@
 MailRobber.configure do |config|
-  config.enabled        = true
-  config.block_delivery = false
+
+  # Enable mail interception (true/false)
+  # When enabled, all emails will be captured and
+  # stored on www.mailrobber.com/yourapikey
+  config.enabled        = !Rails.env.production?
+
+  # Stop emails from being sent via selected delivery mechanism
+  # Set to false (or !Rails.env.production?)
+  # to send emails when on production environment
+  config.block_delivery = true
+
+  # Your app token (generated automatically)
+  # Use rails g mail_robber:install to generate
+  # a new initializer file with a new token
+  # Use rake mail_robber:open to open url with this token
   config.token          = '<%= token %>'
 end
